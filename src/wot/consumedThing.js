d02bb2b22beb540dc9a77c74a23f4a03a6ce804b
Jonas Rook <Jonas.Rook@googlemail.com>
2016-06-27 00:36:44 +0200

    Modified: consumedThing - added the getProperty function

--- src/wot/consumedThing.js
+++ src/wot/consumedThing.js
@@ -2,12 +2,12 @@
 'use strict'
 
 class ConsumedThing {
-    let pool = []
-    let description = {}
-
-    constructor(description, pool) {
-        pool = pool
-        description = description
+    constructor(description, pool, discoverAPI) {
+        console.log(`[${description.name}-Thing] constructor`)
+        this.pool = pool
+        this.description = description
+        this.discoverAPI = discoverAPI
+        this._peripheral = {}
     }
 
     invokeAction(actionName, parameter) {
@@ -28,7 +28,39 @@ class ConsumedThing {
 
     getProperty(name) {
         let promise = new Promise((resolve, reject) => {
-            reject('getProperty is not implemented')
+            console.log(`[${this.description.name}-Thing] getProperty ${name}`)
+
+            let serviceUUID = ''
+            let characteristicUUID = ''
+
+            this.discoverAPI.connectToPeripheral(this._peripheral)
+                .then(() => {
+                    return this.checkName(name)
+                })
+                .then((response) => {
+                    serviceUUID = response.hrefs[0]
+                    characteristicUUID = response.hrefs[1]
+
+                    return this.getPeripheralService(serviceUUID)
+                })
+                .then((response) => {
+                    return this.getServiceCharacteristic(response[0], characteristicUUID)
+                })
+                .then((response) => {
+                    return this.discoverAPI.readCharacteristic(response[0])
+                        .then((response) => {
+                            return response
+                        })
+                })
+                .then((response) => {
+                    this.discoverAPI.disconnectFromPeripheral(this._peripheral)
+                        .then(() => {
+                            resolve(response)
+                        })
+                })
+                .catch((error) => {
+                    console.log(`[${this.description.name}-Thing_getProperty] ${error}`)
+                })
         })
 
         return promise
@@ -65,4 +97,90 @@ class ConsumedThing {
 
         return promise
     }
+
+
+    /******************************************
+    *
+    *              Helper-Functions
+    *
+    ******************************************/
+    addToPool(peripheral){
+
+    }
+
+    removeFromPool(peripheral){
+
+    }
+
+    /**
+    *   checks if the name exists in the description
+    */
+    checkName(name) {
+        console.log(`[${this.description.name}-Thing] checkName ${name}`)
+
+        let promise = new Promise((resolve, reject) => {
+            let propertyObject = this.description.properties.find(property => property.name === name)
+            console.log(`[${this.description.name}-Thing] property ${propertyObject}`)
+
+            if(propertyObject)
+                resolve(propertyObject)
+            else
+                reject(`${name} not found`)
+        })
+
+        return promise
+    }
+
+    /**
+    *   returns the service which includes the characteristic/property
+    */
+    getPeripheralService(propertyServiceUUID) {
+        console.log(`[${this.description.name}-Thing] getPeripheralService ${propertyServiceUUID}`)
+
+        let promise = new Promise((resolve, reject) => {
+            let propertyService = this._peripheral.services.find(service => service.uuid === propertyServiceUUID)
+
+            this.discoverAPI.discoverServices(this._peripheral, propertyServiceUUID)
+                .then((response) => {
+                    if(response)
+                        resolve(response)
+                    else
+                        reject(`${propertyService} not found`)
+                })
+                .catch((error) => {
+                    console.log(`[ConsumedThing_getPeripheralService] ${error}`)
+                })
+        })
+
+        return promise
+    }
+
+    /**
+    *   returns the characteristic which includes the characteristic/property value
+    */
+    getServiceCharacteristic(propertyService, characteristicUUID) {
+        console.log(`[${this.description.name}-Thing] getServiceCharacteristic ${characteristicUUID}`)
+
+        let promise = new Promise((resolve, reject) => {
+                this.discoverAPI.discoverCharacteristics(propertyService, characteristicUUID)
+                    .then((response) => {
+                        if(response)
+                            resolve(response)
+                        else
+                            reject(`${characteristicUUID} not found`)
+                    })
+                    .catch((error) => {
+                        reject(error)
+                    })
+        })
+
+        return promise
+    }
+
+    set peripheral(peripheral){
+        console.log('setperipheral')
+        this._peripheral = peripheral
+    }
 }
+
+module.exports = ConsumedThing;
