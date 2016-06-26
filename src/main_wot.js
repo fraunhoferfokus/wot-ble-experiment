7d8259638d9acc32e74bc57da421e95406f8766f
Jonas Rook <Jonas.Rook@googlemail.com>
2016-06-27 00:35:03 +0200

    New: main_wot - file to call the WoT_API functions

---
+++ src/main_wot.js
@@ -0,0 +1,32 @@
+// to enable es2015 features
+'use strict'
+
+let wot = require('./wot/wot')
+
+console.log('[main_wot] init wot test calls')
+
+let wotAPI = new wot()
+wotAPI.discover()
+    .then((things) => {
+        console.log('[main_wot] list: ')
+
+        return things[0]
+    })
+    .then((thing) => {
+        thing.getProperty('softwareRevision')
+            .then((response) => {
+                console.log(`[main_wot] value 1: ${response}`)
+            })
+            .then(() => {
+                thing.getProperty('softwareRevision')
+                    .then((response) => {
+                        console.log(`[main_wot] value 2: ${response}`)
+                    })
+            })
+            .catch((error) => {
+                console.log(`[main_wot] ${error}`)
+            })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
