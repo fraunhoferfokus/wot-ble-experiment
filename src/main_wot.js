c41435f644accf13c44dfb3e51cb033c1ac92127
Jonas Rook <Jonas.Rook@googlemail.com>
2016-07-01 00:04:13 +0200

    Modified: main_wot - reduced test calls

--- src/main_wot.js
+++ src/main_wot.js
@@ -7,23 +7,12 @@ console.log('[main_wot] init wot test calls')
 
 wot.discover()
     .then((things) => {
-        console.log('[main_wot] list: ')
-
         return things[0]
     })
     .then((thing) => {
         thing.getProperty('softwareRevision')
             .then((response) => {
                 console.log(`[main_wot] value 1: ${response}`)
-            })
-            .then(() => {
-                thing.getProperty('softwareRevision')
-                    .then((response) => {
-                        console.log(`[main_wot] value 2: ${response}`)
-                    })
-            })
-            .catch((error) => {
-                console.log(`[main_wot] ${error}`)
             })
     })
     .catch((error) => {
